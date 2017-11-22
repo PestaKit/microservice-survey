d32b86f0fef6a2f028274260ceb55a11c4cbb788
marcoadrien <adrien.marco@heig-vd.ch>
2017-11-22 16:45:15 +0100

    rajout de tests sur les questions

--- survey-specs/src/test/java/io/pestakit/survey/api/spec/steps/CreationSteps.java
+++ survey-specs/src/test/java/io/pestakit/survey/api/spec/steps/CreationSteps.java
@@ -12,10 +12,8 @@ import io.pestakit.survey.api.dto.Choice;
 import io.pestakit.survey.api.dto.Question;
 import io.pestakit.survey.api.spec.helpers.Environment;
 
-import javax.xml.stream.Location;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -28,13 +26,13 @@ public class CreationSteps {
     private Environment environment;
     private DefaultApi api;
 
-    Object id;
     Question question;
     Object location;
     long questionId;
     Question questionPosted;
     Question questionGetted;
     ArrayList<Question> listOfAllQuestions;
+    int lastQuantityOfQuestions;
 
     private ApiResponse lastApiResponse;
     private ApiException lastApiException;
@@ -46,6 +44,13 @@ public class CreationSteps {
         this.api = environment.getApi();
     }
     //------------------------------------------------------------------------------------------------------------------
+    @Given("^I have getted all the questions and I know the number of questions$")
+    public void i_have_getted_all_the_questions_and_I_know_the_number_of_questions() throws Throwable {
+        i_GET_it_to_the_questions_endpoint();
+        lastQuantityOfQuestions = listOfAllQuestions.size();
+    }
+
+
     @Given("^there is a Survey server$")
     public void there_is_a_Survey_server() throws Throwable {
         assertNotNull(api);
@@ -61,7 +66,47 @@ public class CreationSteps {
     public void i_have_a_question_with_missing_enabled_attribute_in_payload() throws Throwable {
         question = new io.pestakit.survey.api.dto.Question();
         question.setTitle("test2");
-        //question.setEnabled(1);
+        question.setUsed(0);
+        Choice choice1 = new Choice();
+        choice1.setPosition(1);
+        choice1.setText("otpion1");
+        Choice choice2 = new Choice();
+        choice2.setPosition(2);
+        choice2.setText("option2");
+        List<Choice> choiceList = new ArrayList<>();
+        choiceList.add(choice1);choiceList.add(choice2);
+        question.setChoices(choiceList);
+    }
+
+    @Given("^I have a question with missing used attribute in payload$")
+    public void i_have_a_question_with_missing_used_attribute_in_payload() throws Throwable {
+        question = new io.pestakit.survey.api.dto.Question();
+        question.setTitle("test2");
+        question.setEnabled(1);
+        Choice choice1 = new Choice();
+        choice1.setPosition(1);
+        choice1.setText("otpion1");
+        Choice choice2 = new Choice();
+        choice2.setPosition(2);
+        choice2.setText("option2");
+        List<Choice> choiceList = new ArrayList<>();
+        choiceList.add(choice1);choiceList.add(choice2);
+        question.setChoices(choiceList);
+    }
+
+    @Given("^I have a question with missing choices attribute in payload$")
+    public void i_have_a_question_with_missing_choices_attribute_in_payload() throws Throwable {
+        question = new io.pestakit.survey.api.dto.Question();
+        question.setTitle("test2");
+        question.setEnabled(1);
+        question.setUsed(0);
+    }
+
+    @Given("^I have a question with missing title attribute in payload$")
+    public void i_have_a_question_with_missing_title_attribute_in_payload() throws Throwable {
+        question = new io.pestakit.survey.api.dto.Question();
+        //question.setTitle("test2");
+        question.setEnabled(1);
         question.setUsed(0);
         Choice choice1 = new Choice();
         choice1.setPosition(1);
@@ -108,6 +153,14 @@ public class CreationSteps {
     }
 
 //----------------------------------------------------------------------------------------------------------------------
+    @When("^I POST (\\d+) questions successively to the /questions endpoint$")
+    public void i_POST_questions_successively_to_the_questions_endpoint(int numberOfPosts) throws Throwable {
+        i_have_a_question_with_full_payload();
+        for(int i = 0; i < numberOfPosts; i++){
+            i_POST_it_to_the_questions_endpoint();
+        }
+    }
+
 
     @When("^I POST it to the /questions endpoint$")
     public void i_POST_it_to_the_questions_endpoint() throws Throwable {
@@ -161,6 +214,12 @@ public class CreationSteps {
         }
     }
 //----------------------------------------------------------------------------------------------------------------------
+    @Then("^the difference of questions is (\\d+) when I get again all the questions$")
+    public void the_difference_of_questions_is_theGoodDifVariable_when_i_get_again_all_the_questions(int numberOfPosts) throws Throwable {
+        i_GET_it_to_the_questions_endpoint();
+        assertEquals(lastQuantityOfQuestions+numberOfPosts, listOfAllQuestions.size());
+    }
+
     @Then("^I receive a (\\d+) status code$")
     public void i_receive_a_status_code(int statusCode) throws Throwable {
         assertEquals(statusCode, lastStatusCode);
--- survey-specs/src/test/resources/scenarios/creation.feature
+++ survey-specs/src/test/resources/scenarios/creation.feature
@@ -40,8 +40,35 @@ Feature: questions
     Then I receive a 201 status code
 
 
-    #6 #adrien
+  #6 #adrien
   Scenario: create a question with missing enabled attribute in payload
     Given I have a question with missing enabled attribute in payload
     When I POST it to the /questions endpoint
     Then I receive a 400 status code
+
+   #7 #adrien
+  Scenario: create a question with missing title attribute in payload
+    Given I have a question with missing title attribute in payload
+    When I POST it to the /questions endpoint
+    Then I receive a 400 status code
+
+
+   #8 #adrien #PASSE PAS POUR LE MOMENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  #Scenario: create a question with missing used attribute in payload
+   # Given I have a question with missing used attribute in payload
+    #When I POST it to the /questions endpoint
+    #Then I receive a 201 status code
+
+
+   #9 #adrien #PASSE PAS POUR LE MOMENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  #Scenario: create a question with missing choices attribute in payload
+   # Given I have a question with missing choices attribute in payload
+    #When I POST it to the /questions endpoint
+    #Then I receive a 400 status code
+
+
+  #10 #adrien
+  Scenario: get all questions, count them, post some (variable value) more questions and get again all the questions to notice de difference
+    Given I have getted all the questions and I know the number of questions
+    When I POST 2 questions successively to the /questions endpoint
+    Then the difference of questions is 2 when I get again all the questions
