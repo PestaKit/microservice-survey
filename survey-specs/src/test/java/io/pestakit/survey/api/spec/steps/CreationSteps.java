1382dc34aa51f494e81d43f6b3da8604759f29b8
marcoadrien <adrien.marco@heig-vd.ch>
2017-11-22 17:38:24 +0100

    rajout de methodes permettant certains tests pour les surveys

--- survey-specs/src/test/java/io/pestakit/survey/api/spec/steps/CreationSteps.java
+++ survey-specs/src/test/java/io/pestakit/survey/api/spec/steps/CreationSteps.java
@@ -10,6 +10,7 @@ import io.pestakit.survey.ApiResponse;
 import io.pestakit.survey.api.DefaultApi;
 import io.pestakit.survey.api.dto.Choice;
 import io.pestakit.survey.api.dto.Question;
+import io.pestakit.survey.api.dto.Survey;
 import io.pestakit.survey.api.spec.helpers.Environment;
 
 import java.util.ArrayList;
@@ -26,13 +27,18 @@ public class CreationSteps {
     private Environment environment;
     private DefaultApi api;
 
+    long surveyId;
+    Survey survey;
     Question question;
     Object location;
     long questionId;
     Question questionPosted;
     Question questionGetted;
+    Survey surveyPosted;
+    Survey surveyGetted;
     ArrayList<Question> listOfAllQuestions;
     int lastQuantityOfQuestions;
+    ArrayList<String> questionsUrls;
 
     private ApiResponse lastApiResponse;
     private ApiException lastApiException;
@@ -136,6 +142,15 @@ public class CreationSteps {
         question.setChoices(choiceList);
     }
 
+    @Given("^I have a survey with full payload$")
+    public void i_have_a_survey_with_full_payload() throws Throwable {
+        survey = new Survey();
+        i_POST_questions_successively_to_the_questions_endpoint(3);
+        survey.setQuestionURLs(questionsUrls);
+        survey.setTitle("survey test1");
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
 
     @Given("^I have a question with empty payload$")
     public void i_have_a_question_with_empty_payload() throws Throwable {
@@ -152,12 +167,25 @@ public class CreationSteps {
         questionId = Integer.parseInt(idStr);
     }
 
+    @Given("^I have a correct id that exists because I posted a survey")
+    public void i_have_a_correct_id_that_exists_because_i_posted_a_survey() throws Throwable {
+        i_have_a_survey_with_full_payload();
+        i_POST_it_to_the_surveys_endpoint();
+        String locationStr = location.toString();
+        String idStr = locationStr.substring(locationStr.lastIndexOf('/') + 1);
+        idStr = idStr.substring(0, idStr.length() - 1);
+        surveyId = Integer.parseInt(idStr);
+    }
+
 //----------------------------------------------------------------------------------------------------------------------
     @When("^I POST (\\d+) questions successively to the /questions endpoint$")
     public void i_POST_questions_successively_to_the_questions_endpoint(int numberOfPosts) throws Throwable {
         i_have_a_question_with_full_payload();
+        questionsUrls = new ArrayList<>();
         for(int i = 0; i < numberOfPosts; i++){
             i_POST_it_to_the_questions_endpoint();
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            questionsUrls.add(location.toString());
         }
     }
 
@@ -179,6 +207,22 @@ public class CreationSteps {
         }
     }
 
+    @When("^I POST it to the /surveys endpoint$")
+    public void i_POST_it_to_the_surveys_endpoint() throws Throwable {
+        try {
+            lastApiResponse = api.createSurveyWithHttpInfo(survey);
+            surveyPosted = survey;
+            lastApiCallThrewException = false;
+            lastApiException = null;
+            lastStatusCode = lastApiResponse.getStatusCode();
+            location = lastApiResponse.getHeaders().get("Location");
+        } catch (ApiException e) {
+            lastApiCallThrewException = true;
+            lastApiResponse = null;
+            lastApiException = e;
+            lastStatusCode = lastApiException.getCode();
+        }
+    }
 
 
     @When("^I GET it to the /questions endpoint$")
@@ -213,6 +257,23 @@ public class CreationSteps {
             lastStatusCode = lastApiException.getCode();
         }
     }
+
+
+    @When("^I GET it to the /surveys/id_survey endpoint$")
+    public void i_GET_it_to_the_surveys_id_endpoint() throws Throwable {
+        try {
+            lastApiResponse = api.findSurveyByIdWithHttpInfo(surveyId);
+            lastApiCallThrewException = false;
+            lastApiException = null;
+            lastStatusCode = lastApiResponse.getStatusCode();
+            surveyGetted = (Survey) lastApiResponse.getData();
+        } catch (ApiException e) {
+            lastApiCallThrewException = true;
+            lastApiResponse = null;
+            lastApiException = e;
+            lastStatusCode = lastApiException.getCode();
+        }
+    }
 //----------------------------------------------------------------------------------------------------------------------
     @Then("^the difference of questions is (\\d+) when I get again all the questions$")
     public void the_difference_of_questions_is_theGoodDifVariable_when_i_get_again_all_the_questions(int numberOfPosts) throws Throwable {
@@ -228,6 +289,11 @@ public class CreationSteps {
     @And("^The getted question and the posted question are the same$")
     public void the_getted_question_and_the_posted_question_are_the_same() throws Throwable {
         assertEquals(questionPosted, questionGetted);
+    }
+
+    @And("^The getted survey and the posted survey are the same$")
+    public void the_getted_survey_and_the_posted_survey_are_the_same() throws Throwable {
+        assertEquals(surveyPosted, surveyGetted);
     }
 
 
