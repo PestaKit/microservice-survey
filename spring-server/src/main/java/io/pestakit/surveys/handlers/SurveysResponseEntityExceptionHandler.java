07db23bb09911f588967f2bca0634e1bf965a4f6
Miladi Ali <ali.miladi@hes-so.ch>
2017-12-01 09:56:42 +0100

    Modified Error DTO and added a ErroneousField DTO

--- spring-server/src/main/java/io/pestakit/surveys/handlers/SurveysResponseEntityExceptionHandler.java
+++ spring-server/src/main/java/io/pestakit/surveys/handlers/SurveysResponseEntityExceptionHandler.java
@@ -1,28 +1,24 @@
 package io.pestakit.surveys.handlers;
 
 import io.pestakit.surveys.api.exceptions.EmptyListException;
-import io.pestakit.surveys.api.exceptions.IllegalChoicesSizeException;
 import io.pestakit.surveys.api.exceptions.IllegalIdException;
 import io.pestakit.surveys.api.exceptions.IllegalQuestionUrlException;
 import io.pestakit.surveys.model.Error;
-import io.pestakit.surveys.model.ErrorsList;
-import io.pestakit.surveys.validators.QuestionValidator;
 import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.ObjectError;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.ResponseEntity.badRequest;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.http.ResponseEntity.unprocessableEntity;
 
 /**
@@ -35,22 +31,15 @@ public class SurveysResponseEntityExceptionHandler extends ResponseEntityExcepti
         super();
     }
 
-    //
-//    @Override
-//    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-//                                                                  HttpHeaders headers,
-//                                                                  HttpStatus status,
-//                                                                  WebRequest request) {
-//        Error error = new Error();
-//        error.setCode(400);
-//        error.setException(ex.toString());
-//        error.setMessage(ex.getMessage());
-//        error.setName("Caution !! You posted an empty content");
-//        error.setTimestamp(DateTime.now());
-//        error.setPath(request.getContextPath());
-//        return badRequest().body(error);
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        return unprocessableEntity().body(newError(ex, ex.getMessage(), DateTime.now(), new ArrayList<String>()));
+    }
 
 
     @Override
@@ -58,21 +47,19 @@ public class SurveysResponseEntityExceptionHandler extends ResponseEntityExcepti
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
                                                                   WebRequest request) {
-//        Error error = newError(422, ex, ex.getMessage(), DateTime.now());
-        ErrorsList errorsList = new ErrorsList();
-        for (ObjectError objectError : ex.getBindingResult().getAllErrors()) {
-            int code = 422;
-            errorsList.add(newError(ex, objectError.getDefaultMessage(), DateTime.now(), objectError.getObjectName()));
+        List<String> fields = new ArrayList<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            fields.add(fieldError.getField());
         }
-        return unprocessableEntity().body(errorsList);
+        Error error = newError(ex, ex.getMessage(), DateTime.now(), fields);
+        return unprocessableEntity().body(error);
     }
 
-//    }
 
     @ExceptionHandler(EmptyListException.class)
     protected ResponseEntity<Object> handleEmptyList(EmptyListException ex, WebRequest request) {
         Error error = newError(ex, ex.getMessage(),
-                DateTime.now(), "questionURLs");
+                DateTime.now(), new ArrayList<String>());
 
         return unprocessableEntity().body(error);
     }
@@ -87,22 +74,22 @@ public class SurveysResponseEntityExceptionHandler extends ResponseEntityExcepti
 
     @ExceptionHandler(IllegalQuestionUrlException.class)
     protected ResponseEntity<Object> handleIllegalQuestionUrl(IllegalQuestionUrlException ex, WebRequest request) {
-        Error error = newError(ex, ex.getMessage(), DateTime.now(), "questionURLMalformed");
+        Error error = newError(ex, ex.getMessage(), DateTime.now(), new ArrayList<String>());
         return unprocessableEntity().body(error);
     }
 
     @ExceptionHandler(IllegalIdException.class)
     protected ResponseEntity<Object> handleIllegalId(IllegalIdException ex, WebRequest request) {
-        Error error = newError(ex, ex.getMessage(), DateTime.now(), "questionId");
+        Error error = newError(ex, ex.getMessage(), DateTime.now(), new ArrayList<String>());
         return unprocessableEntity().body(error);
     }
 
-    private Error newError(Exception exception, String message, DateTime timestamp, String field) {
+    private Error newError(Exception exception, String message, DateTime timestamp, List<String> fields) {
         Error error = new Error();
         error.setException(exception.toString());
         error.setMessage(exception.getMessage());
         error.setTimestamp(DateTime.now());
-        error.setField(field);
+        error.setFields(fields);
         return error;
     }
 
