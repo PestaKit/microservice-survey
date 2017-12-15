44a6f568ac11523b90494e5e22f19ab0e19feee5
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2017-12-15 16:11:08 +0100

    Fix issues with build scripts, for integration

--- build-image.sh
+++ build-image.sh
@@ -8,3 +8,9 @@ cp target/swagger-spring-1.0.0.jar ../images/survey-server
 echo "******************** Building the image ********************"
 cd ../images/survey-server
 docker build --rm -t pestakit/surveys .
+
+echo "******************** Building the mysql image ********************"
+cd ../../images/mysql
+docker build --rm -t pestakit/surveys-db .
+
+echo
