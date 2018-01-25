bde6d908f1cb3aaf033b78624a22cdd58f3fa2a7
Ali Miladi <ali.miladi@heig-vd.ch>
2018-01-25 10:32:47 +0100

    Added authentication to results endpoints

--- spring-server/src/main/java/io/pestakit/surveys/api/endpoints/ResultsApiController.java
+++ spring-server/src/main/java/io/pestakit/surveys/api/endpoints/ResultsApiController.java
@@ -7,6 +7,7 @@ import io.pestakit.surveys.repositories.AnswersRepository;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -21,6 +22,7 @@ public class ResultsApiController implements ResultsApi {
     @Autowired
     AnswersRepository answersRepository;
 
+    @PreAuthorize("hasRole('USER')")
     @Override
     public ResponseEntity<List<Result>> findResultsOfSurvey(
             @ApiParam(value = "ID of the survey on which we calculate the stats", required = true)
